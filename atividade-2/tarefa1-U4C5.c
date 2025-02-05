f30ad27399d1e838271705df15edab48ab702365
Naylane Ribeiro <naylaneribeiro4@gmail.com>
2025-02-04 22:11:01 -0300

    🐛 fix: lógica atualizada

--- atividade-2/tarefa1-U4C5.c
+++ atividade-2/tarefa1-U4C5.c
@@ -6,18 +6,14 @@
 #define GREEN_PIN 11
 #define BUTTON_PIN 5
 
-//bool led_on = false;        // Variável global para armazenar o estado do LED (não utilizada neste código).
-//bool led_active = false;    // Indica se o LED está atualmente aceso (para evitar múltiplas ativações).
-//absolute_time_t turn_off_time;  // Variável para armazenar o tempo em que o LED deve ser desligado (não utilizada neste código).
-static volatile bool busy = false;            ///< Flag de bloqueio durante operação
 static volatile uint32_t last_press_time = 0; ///< Timestamp do último pressionamento válido
-
+static volatile bool sys_free = true;  
 
 // Função de callback para desligar o LED após o tempo programado.
 int64_t turn_off_callback(alarm_id_t id, void *user_data) {
-    uint32_t led_state = (uint32_t)user_data;
+    uint8_t sequence = (uint8_t)user_data;
 
-    switch (led_state) {
+    switch (sequence) {
         case 1:
             gpio_put(BLUE_PIN, 0);
             add_alarm_in_ms(3000, turn_off_callback, (void*)2, false);
@@ -25,31 +21,30 @@ int64_t turn_off_callback(alarm_id_t id, void *user_data) {
         case 2:
             gpio_put(RED_PIN, 0);
             add_alarm_in_ms(3000, turn_off_callback, (void*)3, false);
+            break;
         case 3:
             gpio_put(GREEN_PIN, 0);
-            busy = false;
+            sys_free = true;
             break;
     }
 
     return 0;
 }
 
-void button_isr(uint gpio, uint32_t events) {
-    if (gpio != BUTTON_PIN) return;
-
+void btn_interrupt(uint gpio, uint32_t events) {
     uint32_t now = to_ms_since_boot(get_absolute_time());
     
     // Verificação de debounce e estado ocupado
-    if ((now - last_press_time) > 250 && !busy) {
-        busy = true;  // Bloqueia sistema durante operação
+    if ((now - last_press_time) > 250 && sys_free) {
+        sys_free = false;  // Bloqueia sistema durante operação
         
         // Ativa todos os LEDs
-        gpio_put(BLUE_PIN, 1);
         gpio_put(RED_PIN, 1);
         gpio_put(GREEN_PIN, 1);
+        gpio_put(BLUE_PIN, 1);        
         
-        // Agenda um alarme para desligar os LEDs após 3 segundos.
-        add_alarm_in_ms(3000, turn_off_callback, (void*)1, false);
+        // Agenda um alarme para desligar os LEDs após 3 segundos. A primeira sequência é então acionada.
+        add_alarm_in_ms(3000, turn_off_callback, 1, false);
         
         last_press_time = now;  // Atualiza timestamp
     }
@@ -77,10 +72,10 @@ int main() {
     gpio_pull_up(BUTTON_PIN); // botão não pressionado = nível alto
 
     // Configuração de interrupção
-    gpio_set_irq_enabled_with_callback(BUTTON_PIN, GPIO_IRQ_EDGE_FALL, true, &button_isr);
+    gpio_set_irq_enabled_with_callback(BUTTON_PIN, GPIO_IRQ_EDGE_FALL, true, &btn_interrupt);
 
     while (true) {
-        sleep_ms(150);
+        sleep_ms(50);
     }
 
     return 0;
